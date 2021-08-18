ca169885e712741681f2874e4eebaca753da7205
龚琳杰 <gonglinjie@baixing.com>
2021-08-18 09:43:28 +0800

    initial commit

---
+++ src/main/java/org/leo/algorithm/BinarySearch.java
@@ -0,0 +1,27 @@
+package org.leo.algorithm;
+
+/**
+ * @ClassName BinarySearch.java
+ * @Author Leo
+ * @Description
+ * @Date 2021/8/18
+ */
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        while (l <= r) {
+            int p = l + (r - l) / 2;
+            if (nums[p] == target) {
+                return p;
+            } else if (nums[p] > target) {
+                r = p - 1;
+            } else {
+                l = p + 1;
+            }
+        }
+        return -1;
+    }
+
+}
