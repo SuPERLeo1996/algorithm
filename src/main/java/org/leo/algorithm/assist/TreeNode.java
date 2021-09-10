a022718e33e4387ab2b8638f1c9e18dd6b4980e2
龚琳杰 <gonglinjie@baixing.com>
2021-09-10 10:21:00 +0800

    hot 100 and offer

---
+++ src/main/java/org/leo/algorithm/assist/TreeNode.java
@@ -0,0 +1,17 @@
+package org.leo.algorithm.assist;
+
+/**
+ * @ClassName TreeNode.java
+ * @Author Leo
+ * @Description
+ * @Date 2021/9/6
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
